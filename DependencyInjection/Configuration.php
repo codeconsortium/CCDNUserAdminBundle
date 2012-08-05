701d7d2982f9b471db9921ec86cd4f81dce7c852
Reece Fowell <reece@codeconsortium.com>
2012-08-05 05:41:08 +0100

    Update documentation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the CCDN UserAdminBundle
+ * This file is part of the CCDNUser AdminBundle
  *
  * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
  *
@@ -13,8 +13,8 @@
 
 namespace CCDNUser\AdminBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 /**
--- Form/Type/AdministrateAccountType.php
+++ Form/Type/AdministrateAccountType.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the CCDN UserAdminBundle
+ * This file is part of the CCDNUser AdminBundle
  *
  * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
  *
