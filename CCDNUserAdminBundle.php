bfc3d178d6663124e9fcd2fffbc305ad367d608c
Reece Fowell <reece@codeconsortium.com>
2012-10-15 00:21:31 +0100

    Switch to twig globals from previous param twig extension.

--- CCDNUserAdminBundle.php
+++ CCDNUserAdminBundle.php
@@ -22,4 +22,55 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class CCDNUserAdminBundle extends Bundle
 {
+			
+	/**
+	 *
+	 * @access public
+	 */
+	public function boot()
+	{
+		$twig = $this->container->get('twig');	
+		$twig->addGlobal('ccdn_user_admin', array(
+			'seo' => array(
+				'title_length' => $this->container->getParameter('ccdn_user_admin.seo.title_length'),
+			),
+			'activation' => array(
+				'show_unactivated_users' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_admin.activation.show_unactivated_users.layout_template'),
+					'member_since_datetime_format' => $this->container->getParameter('ccdn_user_admin.activation.show_unactivated_users.member_since_datetime_format'),
+				),
+			),
+			'ban' => array(
+				'show_banned_users' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_admin.ban.show_banned_users.layout_template'),
+					'member_since_datetime_format' => $this->container->getParameter('ccdn_user_admin.ban.show_banned_users.member_since_datetime_format'),
+				),
+			),
+			'account' => array(
+				'show_newest_users' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_admin.account.show_newest_users.layout_template'),
+					'member_since_datetime_format' => $this->container->getParameter('ccdn_user_admin.account.show_newest_users.member_since_datetime_format'),
+				),
+				'show_user' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_admin.account.show_user.layout_template'),
+					'member_since_datetime_format' => $this->container->getParameter('ccdn_user_admin.account.show_user.member_since_datetime_format'),
+				),
+				'edit_user_account' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_admin.account.edit_user_account.layout_template'),
+					'form_theme' => $this->container->getParameter('ccdn_user_admin.account.edit_user_account.form_theme'),
+				),
+				'edit_user_profile' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_admin.account.edit_user_profile.layout_template'),
+					'form_theme' => $this->container->getParameter('ccdn_user_admin.account.edit_user_profile.form_theme'),
+				),
+			),
+			'role' => array(
+				'set_users_role' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_admin.role.set_users_role.layout_template'),
+					'form_theme' => $this->container->getParameter('ccdn_user_admin.role.set_users_role.form_theme'),
+				),
+			),
+		));
+	}
+	
 }
